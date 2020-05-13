46ebaa958594ca1894a3bffbdf0802efc8aa7bc3
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-05-13 23:41:30 +0300

    Added 5.Recursion chapter brief notes

---
+++ src/recursion.hs
@@ -0,0 +1,73 @@
+-- Recursion
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+-- Wow so elegant!
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+-- my realization (1)
+take' :: Int -> [b] -> [b]
+take' n all@(x:xs)
+    | n <= 0             = []
+    | length (all) < n   = []
+    | otherwise          = x : take' (n-1) xs
+
+-- book realization (1)
+take'' :: (Num i, Ord i) => i -> [a] -> [a]
+take'' n _
+    | n <= 0   = []
+take'' _ []     = []
+take'' n (x:xs) = x : take'' (n-1) xs
+
+-- my realization (2)
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) =  reverse' xs ++ [x]
+
+-- book realization (2)
+reverse'' :: [a] -> [a]
+reverse'' [] = []
+reverse'' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+-- my realization (3)
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = elem' a xs
+
+-- book realization (3)
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
+
+-- Mind blowing at first glance
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in  smallerSorted ++ [x] ++ biggerSorted
