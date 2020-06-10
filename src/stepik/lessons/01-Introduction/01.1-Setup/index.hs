3d7a723afe20531169f8d02e4189b2250b1e1e10
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-06-10 22:30:27 +0300

    feat: Added `01-Introduction` chapter & `01.1-Setup` sub-chapter

---
+++ src/stepik/lessons/01-Introduction/01.1-Setup/index.hs
@@ -0,0 +1,14 @@
+-- Tab `    ` is equal to 8 ` ` spaces
+
+-- ghci is a main command to run `glasgow haskell compiler interactive`
+
+-- ghci's prompt can be changed by `:set prompt "Awesome prompt> " command
+
+-- To load file pass its name to a ghci Hello.hs
+
+-- load module
+-- `:load Hello.hs`
+-- `:l Hello.hs`
+
+-- `:reload`
+-- `:r`
