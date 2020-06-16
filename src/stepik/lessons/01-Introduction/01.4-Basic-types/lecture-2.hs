a19bdc155b3c3203647a8add86d3221ae98418d1
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-06-17 00:24:42 +0300

    feat(01-Introduction): Added `lecture-2.hs` from `01.4-Basic-types` chapter

---
+++ src/stepik/lessons/01-Introduction/01.4-Basic-types/lecture-2.hs
@@ -0,0 +1,14 @@
+-- :t 100500
+-- Num p => p
+
+
+-- Explicit type declaration
+x' = 100500 :: Int
+-- :t x
+-- x :: Int
+
+-- Class Num includes: Double, Float, Int, Integer
+-- Class Fractional includes: Double, Float
+
+-- Integer could take number with any length
+a = 1000000000000000000000000000000000000000 :: Integer
