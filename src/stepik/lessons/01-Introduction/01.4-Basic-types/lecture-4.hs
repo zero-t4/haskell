67933dfd8d511892c87a57a2b19cb707f847497a
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-06-17 21:59:21 +0300

    feat(01-Introduction): Added `lecture-4.hs` from `01.4-Basic-types` chapter

---
+++ src/stepik/lessons/01-Introduction/01.4-Basic-types/lecture-4.hs
@@ -0,0 +1,10 @@
+-- To define module use word `module ModuleName where`
+module Test where
+
+-- To import, use next construction `import ModuleName`
+import Data.Char
+
+a = isDigit '7'
+-- True
+
+-- To import module in ghci write `import ModuleName`
