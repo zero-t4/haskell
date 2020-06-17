61bb1db811bdfad022952bf4cf6dd045fe0f0d75
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-06-17 22:14:54 +0300

    feat(01-Introduction): Added `lecture-6.hs` from `01.4-Basic-types` chapter

---
+++ src/stepik/lessons/01-Introduction/01.4-Basic-types/lecture-6.hs
@@ -0,0 +1,17 @@
+-- Lists
+
+a = [1,2,3,4,5]
+-- :t a
+-- a :: [Integer]
+
+b = ['H', 'i'] == "Hi"
+-- True
+
+-- :t ['H', 'i']
+-- ['H', 'i'] :: [Char]
+
+-- :t "Hi"
+-- "Hi" :: [Char]
+
+-- Type `[Char]` can be replaced with `String`
+c = "Hi" :: String
