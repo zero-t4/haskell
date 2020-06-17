0e77f879ca7a698cea1d699842d5ca6812706771
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-06-17 21:54:57 +0300

    feat(01-Introduction): Added `lecture-3.hs` from `01.4-Basic-types` chapter

---
+++ src/stepik/lessons/01-Introduction/01.4-Basic-types/lecture-3.hs
@@ -0,0 +1,9 @@
+-- Function types defined as `funcName :: ArgTypeConstraint -> ReturnType`
+-- :t not
+-- not :: Bool -> Bool
+
+-- :t (&&)
+-- (&&) :: Bool -> Bool -> Bool
+
+-- :t (&& True)
+-- (&& True) :: Bool -> Bool
