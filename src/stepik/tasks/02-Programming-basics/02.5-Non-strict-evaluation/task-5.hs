69c82c14698bbafef820d4173348307c82dc166e
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-04 20:37:11 +0300

    feat(02-Programming-basics): `02.5-Non-strict-evaluation` Added task-5.hs

---
+++ src/stepik/tasks/02-Programming-basics/02.5-Non-strict-evaluation/task-5.hs
@@ -0,0 +1,33 @@
+{-|
+  При вычислении каких из перечисленных ниже функций использование seq предотвратит нарастание количества невычисленных редексов при увеличении значения первого аргумента:
+
+  foo 0 x = x
+  foo n x = let x' = foo (n - 1) (x + 1)
+            in x' `seq` x'
+
+  bar 0 f = f
+  bar x f = let f' = \a -> f (x + a)
+                x' = x - 1
+            in f' `seq` x' `seq` bar x' f'
+
+  baz 0 (x, y) = x + y
+  baz n (x, y) = let x' = x + 1
+                     y' = y - 1
+                     p  = (x', y')
+                     n' = n - 1
+                 in p `seq` n' `seq` baz n' p
+
+  quux 0 (x, y) = x + y
+  quux n (x, y) = let x' = x + 1
+                      y' = y - 1
+                      p  = (x', y')
+                      n' = n - 1
+                  in x' `seq` y' `seq` n' `seq` quux n' p
+-}
+
+{-|
+  foo
+  bar
+  baz
+  quux -- True
+-}
