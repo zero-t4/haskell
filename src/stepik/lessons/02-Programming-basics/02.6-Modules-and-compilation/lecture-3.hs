7e434214df4d6c0dce47418f0bef31ca64ab8b03
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-04 21:14:40 +0300

    feat(02-Programming-basics): `02.6-Modules-and-compilation` Added lecture-3

---
+++ src/stepik/lessons/02-Programming-basics/02.6-Modules-and-compilation/lecture-3.hs
@@ -0,0 +1,12 @@
+-- Order of Haskell code compilation:
+{-|
+  1) Import file namings
+  2) Syntax checking
+  3) Type checking
+  4) Haskell -> Coa
+  5) Code optimization
+  6) Code generation
+  7) STG code machine
+  8) C--
+  9) LLVM | GHC
+-}
