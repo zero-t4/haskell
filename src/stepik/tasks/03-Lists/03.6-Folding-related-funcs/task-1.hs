fd2251c4b0927ff48408a7ff2ba8ae3e15644d56
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-06 21:30:36 +0300

    feat(03-Lists): `03.6-Folding-related-funcs` Added task-1.hs

---
+++ src/stepik/tasks/03-Lists/03.6-Folding-related-funcs/task-1.hs
@@ -0,0 +1,9 @@
+{-|
+  Напишите реализацию функции, возвращающей последний элемент списка, через foldl1.
+
+  lastElem :: [a] -> a
+  lastElem = foldl1 undefined
+-}
+
+lastElem :: [a] -> a
+lastElem = last
