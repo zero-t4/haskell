3ae0a2db555ec84f92676fd210b590344c29a4d9
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-07 23:52:30 +0300

    feat(03-Lists): `03.4-Right-folding` Added more details to task-5.hs

--- src/stepik/tasks/03-Lists/03.4-Right-folding/task-5.hs
+++ src/stepik/tasks/03-Lists/03.4-Right-folding/task-5.hs
@@ -3,3 +3,10 @@
 -}
 
 -- head
+
+-- foldr f ini [1,2,3] ->> 1 `f` (2 `f` (3 `f` ini))
+
+-- foldr const undefined [1,2,3] ->> const 1 (const 2 (const (3 undefined)))
+-- foldr const undefined [1,2,3] ->> const 1 (const 2 3)
+-- foldr const undefined [1,2,3] ->> const 1 2
+-- foldr const undefined [1,2,3] ->> 1
