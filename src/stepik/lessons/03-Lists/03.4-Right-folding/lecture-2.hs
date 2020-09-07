e874655b787f37e22c697bcaa0ad9f36103244c6
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-07 23:41:14 +0300

    feat(03-Lists): `03.4-Right-folding` Removed dead code from lecture-2.hs

--- src/stepik/lessons/03-Lists/03.4-Right-folding/lecture-2.hs
+++ src/stepik/lessons/03-Lists/03.4-Right-folding/lecture-2.hs
@@ -2,20 +2,6 @@ module Demo where
 
 import Prelude hiding (foldr)
 
-sumList :: [Integer] -> Integer
-sumList []      = 0
-sumList (x:xs)  = x + sumList xs
-
-productList :: [Integer] -> Integer
-productList [] = 1
-productList (x:xs) = x * productList xs
-
-concatList :: [[a]] -> [a]
-concatList [] = []
-concatList (x:xs) = x ++ concatList xs
-
-foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr f ini []     = ini
 foldr f ini (x:xs) = x `f` foldr f ini xs
 
 sumPositiveSquares :: [Integer] -> Integer
