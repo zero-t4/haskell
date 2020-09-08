cf8407d0819672fbd2ccdd40d60757bb2876481f
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-08 23:55:46 +0300

    feat(03-Lists): `03.6-Folding-related-funcs` Added lecture-5.hs

---
+++ src/stepik/lessons/03-Lists/03.6-Folding-related-funcs/lecture-5.hs
@@ -0,0 +1,18 @@
+module Demo where
+
+import Prelude hiding (iterate)
+
+unfold :: (b -> (a,b)) -> b -> [a]
+unfold f ini = let (x,ini') = f ini in
+  x : unfold f ini'
+
+{-|
+  iterate f x == [x, f x, f (f x), f (f (f x)), ..]
+
+  iterate :: (a -> a) -> a -> [a]
+-}
+
+iterate f = unfold (\x -> (x, f x))
+
+a = take 5 . iterate (^2) $ 2
+-- [2,4,16,256,65536]
