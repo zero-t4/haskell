c447443e3af2845b2f154726e0e769a9e3d70a2a
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-09 00:39:37 +0300

    feat(04-Data-types): `04.1-Enumeration-types` Added task-1.hs

---
+++ src/stepik/tasks/04-Data-types/04.1-Enumeration-types/task-1.hs
@@ -0,0 +1,10 @@
+{-|
+  Выберите корректные определения типов данных.
+
+  ( ) data myType = A | B | C
+  ( ) data T = Con1 | myCon2
+  (*) data T = Con1 | Con2
+  (*) data T = A
+  (*) data Data = A | B
+  (*) data Type = A | B | C
+-}
