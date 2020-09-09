68942c5d2307ec7cba3298501a68002971ef3ca5
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-09 23:27:10 +0300

    feat(04-Data-types): `04.1-Enumeration-types` Added task-2.hs

---
+++ src/stepik/tasks/04-Data-types/04.1-Enumeration-types/task-2.hs
@@ -0,0 +1,16 @@
+{-|
+  Тип данных Color определен следующим образом
+
+  data Color = Red | Green | Blue
+  Определите экземпляр класса Show для типа Color, сопоставляющий каждому из трех цветов его текстовое представление.
+
+  GHCi> show Red
+  "Red"
+-}
+
+data Color = Red | Green | Blue
+
+instance Show Color where
+    show Red = "Red"
+    show Green = "Green"
+    show Blue = "Blue"
