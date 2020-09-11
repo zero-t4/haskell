a27b1a8a64a0951b6820de6de4ccd3fa1d0e2390
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-11 23:26:51 +0300

    feat(04-Data-types): `04.3-Records-syntax` Added task-1.hs

---
+++ src/stepik/tasks/04-Data-types/04.3-Records-syntax/task-1.hs
@@ -0,0 +1,30 @@
+{-|
+  Определите тип записи, который хранит элементы лога. Имя конструктора должно совпадать с именем типа, и запись должна содержать три поля:
+  timestamp — время, когда произошло событие (типа UTCTime);
+  logLevel — уровень события (типа LogLevel);
+  message — сообщение об ошибке (типа String).
+  Определите функцию logLevelToString, возвращающую текстуальное представление типа LogLevel, и функцию logEntryToString, возвращающую текстуальное представление записи в виде:
+
+  <время>: <уровень>: <сообщение>
+
+
+  Для преобразование типа UTCTime в строку используйте функцию timeToString.
+-}
+
+import Data.Time.Clock
+import Data.Time.Format
+import System.Locale
+
+timeToString :: UTCTime -> String
+timeToString = formatTime defaultTimeLocale "%a %d %T"
+
+data LogLevel = Error | Warning | Info
+logLevelToString a = case a of
+    Error -> "Error"
+    Warning -> "Warning"
+    Info -> "Info"
+
+data LogEntry = LogEntry {timestamp :: UTCTime, logLevel :: LogLevel, message :: String}
+
+logEntryToString :: LogEntry -> String
+logEntryToString a = timeToString(timestamp a) ++ ": " ++ logLevelToString(logLevel a) ++ ": " ++ message a
