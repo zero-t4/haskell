7da36a7e448130d7276528f87881b1867aa67aeb
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-13 00:42:53 +0300

    feat(04-Data-types): `04.4-Types-with-params` Fix task-6.hs

--- src/stepik/tasks/04-Data-types/04.4-Types-with-params/task-6.hs
+++ src/stepik/tasks/04-Data-types/04.4-Types-with-params/task-6.hs
@@ -1,11 +1,31 @@
 {-|
-  Исправьте ошибку в приведенном коде.
+  Реализуйте функцию parsePerson, которая разбирает строки вида firstName = John\nlastName = Connor\nage = 30 и возвращает либо результат типа Person, либо ошибку типа Error.
 
-  eitherToMaybe :: Either a -> Maybe a
-  eitherToMaybe (Left a) = Just a
-  eitherToMaybe (Right _) = Nothing
+  Строка, которая подается на вход, должна разбивать по символу '\n' на список строк, каждая из которых имеет вид X = Y. Если входная строка не имеет указанный вид, то функция должна возвращать ParsingError.
+  Если указаны не все поля, то возвращается IncompleteDataError.
+  Если в поле age указано не число, то возвращается IncorrectDataError str, где str — содержимое поля age.
+  Если в строке присутствуют лишние поля, то они игнорируются.
 -}
 
-eitherToMaybe :: Either a b -> Maybe a
-eitherToMaybe (Left a) = Just a
-eitherToMaybe (Right _) = Nothing
+data Error = ParsingError | IncompleteDataError | IncorrectDataError String
+
+data Person = Person { firstName :: String, lastName :: String, age :: Int }
+
+parsePerson :: String -> Either Error Person
+parsePerson s = makePerson (lineWith "firstName ") (lineWith "lastName ") (lineWith "age ")
+  where
+    info :: [(String, String)]
+    info = map (break (== '=')) . lines $ s
+
+    lineWith :: String -> Maybe String
+    lineWith = flip lookup info
+
+    makePerson :: Maybe String -> Maybe String -> Maybe String -> Either Error Person
+    makePerson (Just firstNameA) (Just lastNameA) (Just ageA) =
+      case (firstNameA, lastNameA, ageA) of
+        ('=' : ' ' : firstName, '=' : ' ' : lastName, '=' : ' ' : age) ->
+          case reads age of
+            [(i, "")] -> Right $ Person firstName lastName i
+            _         -> Left $ IncorrectDataError age
+        _ -> Left ParsingError
+    makePerson _ _ _ = Left IncompleteDataError
