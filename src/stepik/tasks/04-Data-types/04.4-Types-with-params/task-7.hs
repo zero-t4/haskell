9f2a39fd3cfb7b63e4e4fcb712998f94cbe97edf
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-13 00:28:23 +0300

    feat(04-Data-types): `04.4-Types-with-params` Added task-7.hs

---
+++ src/stepik/tasks/04-Data-types/04.4-Types-with-params/task-7.hs
@@ -0,0 +1,14 @@
+{-|
+  Укажите все выражения, имеющие вид *.
+-}
+{-|
+  ( ) Either (Int -> (,)) Int
+  ( ) Nothing
+  (*) Maybe (Int -> Either Int Int)
+  ( ) Either True False
+  (*) Maybe Int -> Int
+  ( ) Maybe -> Int
+  (*) `(Maybe Int, Either (Int -> (Char, Char)) Int)
+  (*) Int -> Int
+  ( ) Either (Int -> Int) Maybe
+-}
